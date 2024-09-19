d746cf85884435942d9e4f34185c6251e972c31a
Mola26 <nokuthaba2608@gmail.com>
2024-09-20 00:19:53 +0200

    Created js file

---
+++ index.js
@@ -0,0 +1,11 @@
+const btnEl = document.getElementById("btn");
+
+const birthdayEl = document.getElementById("btn");
+getElemeneById("birthday");
+
+function CalculateAge() {
+  const birthdayValue = birthdayEl.value;
+  console.log(birthdayValue);
+}
+
+btnElement.addEventListener("click", CalculateAge);
